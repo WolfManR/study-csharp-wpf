f05fc63ad62503a5ba53aad0d8a95c266f0c9b20
Tarxos <ftarxos@gmail.com>
2022-03-12 02:45:23 +0300

    add package for ms hosting and initialize it

--- DeclarativeWPF/DeclarativeWPF.UI/App.xaml.cs
+++ DeclarativeWPF/DeclarativeWPF.UI/App.xaml.cs
@@ -1,8 +1,47 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace DeclarativeWPF.UI
 {
     public partial class App : Application
     {
+        private static IHost? _hosting;
+
+        private bool _isHostingDisposed;
+
+        private static IHost Hosting => _hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            await Hosting.StartAsync();
+
+            this.Exit += DisposeHosting;
+            this.DispatcherUnhandledException += DisposeHosting;
+            this.SessionEnding += DisposeHosting;
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        private void DisposeHosting(object sender, EventArgs e)
+        {
+            this.Exit -= DisposeHosting;
+            this.DispatcherUnhandledException -= DisposeHosting;
+            this.SessionEnding -= DisposeHosting;
+
+            if (_isHostingDisposed) return;
+            _isHostingDisposed = true;
+            using var host = Hosting;
+            host.StopAsync(TimeSpan.FromSeconds(5)).Wait();
+        }
+
+        private static void ConfigureServices(HostBuilderContext arg1, IServiceCollection arg2)
+        {
+        }
     }
 }
