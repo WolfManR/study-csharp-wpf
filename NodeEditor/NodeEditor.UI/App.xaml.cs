7352e40a7dc568285844993e4572280442f6e512
Tarxos <ftarxos@gmail.com>
2022-03-14 01:56:51 +0300

    add ui project for node editor

---
+++ NodeEditor/NodeEditor.UI/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace NodeEditor.UI
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ NodeEditor/NodeEditor.UI/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace NodeEditor.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
