c38949d8c6157c5d7471c57d6befe44376c87332
Tarxos <ftarxos@gmail.com>
2022-04-06 16:24:32 +0300

    add main window content

--- NodeEditor/NodeEditor.UI/MainWindow.xaml.cs
+++ NodeEditor/NodeEditor.UI/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using NodeEditor.UI.Helpers;
+
+using System.Windows;
 
 namespace NodeEditor.UI
 {
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private void OnScrolling(object sender, RoutedEventArgs e) => AttachedAdorner.OnScrolling();
     }
 }
