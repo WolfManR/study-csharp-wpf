aa5932d599e21732485b8695d52391308b45b218
Tarxos <ftarxos@gmail.com>
2022-04-06 15:17:00 +0300

    add sample helpers

---
+++ NodeEditor/NodeEditor.UI/Helpers/GroupBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using NodeEditor.UI.ViewModels;
+using RichCanvas;
+using RichCanvas.Helpers;
+
+namespace NodeEditor.UI.Helpers;
+
+public class GroupBehavior : Behavior<System.Windows.Shapes.Rectangle>
+{
+    private RichItemContainer ItemContainer => VisualHelper.GetParentContainer(AssociatedObject);
+    private Group GroupDataContext => ItemContainer.DataContext as Group;
+    protected override void OnAttached()
+    {
+        ItemContainer.Host.DrawingEnded += Host_DrawingEnded;
+    }
+
+    private void RotateTransform_Changed(object sender, System.EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Host_DrawingEnded(object sender, RoutedEventArgs e)
+    {
+        if (GroupDataContext != null)
+        {
+            List<object> intersectedElements = ItemContainer.Host.GetElementsInArea(new Rect(ItemContainer.Left, ItemContainer.Top, ItemContainer.Width, ItemContainer.Height));
+            GroupDataContext.SetGroupedElements(intersectedElements.OfType<Drawable>().Where(d => !(d is Group) && d is IGroupable).ToArray());
+            GroupDataContext.SetGroupSize();
+        }
+    }
+
+}
